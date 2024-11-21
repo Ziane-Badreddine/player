3a4cb3d107a983d174c668d7076d147c2a0e5ea9
ziane badr eddine <zianebadredddine2004@gmail.com>
2024-11-21 23:28:32 +0100

    '

--- app/_components/TaskList.tsx
+++ app/_components/TaskList.tsx
@@ -7,6 +7,7 @@ import { Search } from "lucide-react";
 import db from "@/prisma/route";
 import { Label } from "@/components/ui/label";
 import ButtonAdd from "./ButtonAdd";
+import { player } from "@/prisma/generated/client";
 
 
 export default async function TaskList() {
@@ -32,7 +33,7 @@ export default async function TaskList() {
                     <Input placeholder="filter..." className="w-[40%]" />
                 </div>
                 <div className="flex items-center justify-center flex-col gap-5 mb-10 w-full">
-                    {players.map((play,index) => {
+                    {players.map((play: player,index) => {
                     return <Task key={index} initialdata={play}/>
                     })}
                 </div>
